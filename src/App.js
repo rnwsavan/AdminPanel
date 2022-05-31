2b53876dbf753432c8a3d2728cb548aa2ffbc238
rnwsavan <rnwsavanasodariya@gmail.com>
2022-05-31 13:30:09 +0530

    Admin Change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { Route, Switch } from "react-router-dom";
-import Lauout from "./Components/Layout/Lauout";
+import Layout_Btn from "./Components/Layout-Btn/Layout_Btn";
+import Layout from "./Components/Layout/Layout";
 import Doctor from "./Containers/Doctors/Doctor";
 import Medicine from "./Containers/Medicine/Medicine";
 
 function App() {
   return (
     <>
-      <Lauout>
+      <Layout>
         <Switch>
           <Route exact path={"/Medicine"} component={Medicine} />
           <Route exact path={"/Doctor"} component={Doctor} />
+          <Route exact path={"/Btn"} component={Layout_Btn}/>
         </Switch>
-      </Lauout>
+      </Layout>
     </>
   );
 }
